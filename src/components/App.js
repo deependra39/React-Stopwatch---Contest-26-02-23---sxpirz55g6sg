966bbae79557ed367f3a3a9cffe5a14a876a0912
deependra39 <113820694+deependra39@users.noreply.github.com>
2023-02-26 15:37:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -16,6 +16,7 @@ const App = () => {
 
   const stopTimer = () => {
     clearInterval(intervalRef.current);
+    setCurrentTime(Date.now() - startTime.current);
   };
 
   const resetTimer = () => {
